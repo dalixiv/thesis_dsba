147f1c0906c3100333ffb6cf4ec4b79e124dc9a2
dalixiv <55178890+dalixiv@users.noreply.github.com>
2023-05-24 21:19:36 +0300

    Add files via upload

---
+++ Word2VecAlgorithm (1).py
@@ -0,0 +1,39 @@
+from surprise import AlgoBase, PredictionImpossible
+from gensim.models import Word2Vec
+from sklearn.preprocessing import normalize
+
+class Word2VecAlgorithm(AlgoBase):
+    def __init__(self, sim_options={}):
+        super().__init__(sim_options=sim_options)
+        self.word2vec_model = Word2Vec()
+        self.item_embeddings = None
+
+    def fit(self, trainset):
+        super().fit(trainset)
+        item_descriptions = [trainset.to_raw_iid(item_id) for item_id in trainset.all_items()]
+        item_descriptions = [item_description.split() for item_description in item_descriptions]
+        self.word2vec_model.build_vocab(item_descriptions)
+        self.word2vec_model.train(item_descriptions, total_examples=len(item_descriptions), epochs=10)
+        item_embedding = normalize(self.word2vec_model.wv[item_description].reshape(1, -1))
+        return self
+    
+    def get_best_item(self, user, item):
+        if not (self.trainset.knows_user(user) and self.trainset.knows_item(item)):
+            raise PredictionImpossible('User or item is unknown.')
+
+        item_description = self.trainset.to_raw_iid(item)
+        item_description = item_description.split()
+        item_embedding = self.word2vec_model.wv[item_description]
+        item_embedding = item_embedding / np.linalg.norm(item_embedding)
+
+        ms = self.similar_by_vector(item, topn= n+1)[1:]
+
+        new_ms = []
+        for j in ms:
+            pair = (products_dict[j[0]][0], j[1])
+            new_ms.append(pair)
+        
+    return best_item     
+
+
+
